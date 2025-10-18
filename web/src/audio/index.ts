f0c2ee71123c06bd9ac837f86e180661e5b5e3c5
emmjayh <emmjayh@users.noreply.github.com>
2025-10-18 10:30:34 -0400

    fix(web): scale monitor gain after worklet

--- web/src/audio/index.ts
+++ web/src/audio/index.ts
@@ -139,7 +139,6 @@ class AudioEngine {
   private audioContext: AudioContext | null = null;
   private workletNode: AudioWorkletNode | null = null;
   private micGainNode: GainNode | null = null;
-  private workletMonitorTap: GainNode | null = null;
   private streamSource: MediaStreamAudioSourceNode | null = null;
   private vadSession: ort.InferenceSession | null = null;
   private pitchSession: ort.InferenceSession | null = null;
@@ -354,16 +353,10 @@ class AudioEngine {
 
     this.streamSource.connect(this.workletNode);
 
-    this.workletMonitorTap?.disconnect();
-    this.workletMonitorTap = this.audioContext.createGain();
-    this.workletMonitorTap.gain.value = 0;
-    this.workletNode.connect(this.workletMonitorTap);
-    this.workletMonitorTap.connect(this.vocalBusNode);
-
     this.micGainNode?.disconnect();
     this.micGainNode = this.audioContext.createGain();
     this.micGainNode.gain.value = this.micMonitorLinear;
-    this.streamSource.connect(this.micGainNode);
+    this.workletNode.connect(this.micGainNode);
     this.micGainNode.connect(this.vocalBusNode);
 
     if (this.workletNode) {
@@ -1536,12 +1529,10 @@ class AudioEngine {
     this.stop();
     this.workletNode?.disconnect();
     this.micGainNode?.disconnect();
-    this.workletMonitorTap?.disconnect();
     this.streamSource?.disconnect();
     this.vocalBusNode?.disconnect();
     this.workletNode = null;
     this.micGainNode = null;
-    this.workletMonitorTap = null;
     this.streamSource = null;
     this.vocalBusNode = null;
 
